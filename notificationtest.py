2a236c139ae469e50935be940cc0d7a867662b82
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2021-04-03 10:03:36 -0400

    Now joins the meet

--- notificationtest.py
+++ notificationtest.py
@@ -1,72 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from time import sleep
-from win10toast import ToastNotifier
+import time
 
 
-import re
-
-#Enter login info 
-#gmail_user = input("Enter your email: ")
-#gmail_password = input("Enter your password: ")
-#meet_link = input("Paste Google Meet Link here: ")
-
-gmail_user = ""
-gmail_password = ""
-meet_link = "https://meet.google.com/pbo-ynqq-kjz"
-#Makes the window suitable for the program
+# Code to allow access for Microphone, Camera and notifications
+# 0 is disable and 1 is allow.
 opt = Options()
 opt.add_argument("--disable-infobars")
 opt.add_argument("start-maximized")
 opt.add_argument("--disable-extensions")
-opt.add_argument("--disable-web-security")
-opt.add_argument("--allow-running-insecure-content")
-opt.add_argument('--disable-blink-features=AutomationControlled')
-
-
-opt.add_experimental_option("prefs", {
-    "profile.default_content_setting_values.media_stream_mic": 1,
-    "profile.default_content_setting_values.media_stream_camera": 1,
-    "profile.default_content_setting_values.geolocation": 1,
-    "profile.default_content_setting_values.notifications": 1
+opt.add_experimental_option("prefs", { \
+"profile.default_content_setting_values.media_stream_mic": 1, 
+"profile.default_content_setting_values.media_stream_camera": 1,
+"profile.default_content_setting_values.geolocation": 1, 
+"profile.default_content_setting_values.notifications": 1 
 })
 
-#Signs in
-chromedriver = webdriver.Chrome(options=opt, executable_path="chromedriver.exe") 
+def join_meet(link):
+    driver.get("https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#identifier")
+    time.sleep(4)
+    #Enters Email
+    driver.find_element_by_xpath("//input[@name='identifier']").send_keys("meetplusplus@gmail.com")
+    time.sleep(2)
+    #Clicks next
+    driver.find_element_by_xpath("//*[@id='identifierNext']/div/button/div[2]").click()
+    time.sleep(5)
+    #Enters password
+    driver.find_element_by_xpath("//input[@name='password']").send_keys("GoogleMeet++")
+    time.sleep(2)
+    #Clicks next
+    driver.find_element_by_xpath("//*[@id='passwordNext']/div/button").click()
+    time.sleep(5)
+    driver.get(link)
+    driver.refresh()
+    time.sleep(5)
+    #Turning off mic and video
+    camandmic = driver.find_elements_by_css_selector('div.U26fgb.JRY2Pb.mUbCce.kpROve')
+    for i in camandmic:
+        i.click()
+    #Joins meeting
+    driver.find_element_by_css_selector('div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+    time.sleep(5)
+    #Goes to chat tab
+    driver.find_element_by_css_selector('div.HKarue').click()
+    time.sleep(10)  
+    messages = driver.find_elements_by_css_selector('div.oIy2qc')
+    for message in messages[1:]:
+        print(message.text)
+    print(messages)
 
-#chromedriver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')  #uses stack overflow to login through google
-#sleep(5)
-#chromedriver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()  #signing in with google
-#chromedriver.find_element_by_xpath('//input[@type="email"]').send_keys(gmail_user)  #Entering the email
-#chromedriver.find_element_by_xpath('//*[@id="identifierNext"]').click()
-#sleep(5)
-#chromedriver.find_element_by_xpath('//input[@type="password"]').send_keys(gmail_password)  #entering the password
-#chromedriver.find_element_by_xpath('//*[@id="passwordNext"]').click()
 
-resume = ""
-while(resume != "continue"):
-    resume = input("Type \"continue\" here when you log into Google: ")
+if __name__ == "__main__":
+    link = "https://meet.google.com/pbo-ynqq-kjz"
+    driver = webdriver.Chrome(chrome_options=opt, executable_path=r'chromedriver.exe') 
+    join_meet(link)
+    
 
-#Joins the meeting
-chromedriver.get(meet_link)
-sleep(15)
-chromedriver.find_element_by_css_selector('button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.ksBjEc.lKxP2d.cjtUbb').click()
-sleep(5)
-camandmic = chromedriver.find_elements_by_css_selector('div.U26fgb.JRY2Pb.mUbCce.kpROve')
-for i in camandmic:
-    i.click()
-chromedriver.find_element_by_css_selector('div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click() 
-sleep(5)
-
-#Goes to chat tab
-chromedriver.find_element_by_css_selector('div.uArJ5e.UQuaGc.kCyAyd.QU4Gid.foXzLb.M9Bg4d').click()
-sleep(5)
-
-#Gets the messages
-names = chromedriver.find_elements_by_css_selector('div.z38b6.CnDs7d.hPqowe')
-print(names)
-
-toaster = ToastNotifier()
-#toaster.show_toast(f"Cum",
-                   #{article},
-                   #duration=10)
